9310245f5e4e01bc7a956833d89a89acf76ddec7
bucurioana23@stud.ase.ro <bucurioana23@stud.ase.ro>
2025-10-19 14:56:50 +0300

    primul commit

---
+++ app/src/main/java/com/example/concertpracticeapp/Concert.java
@@ -0,0 +1,60 @@
+package com.example.concertpracticeapp;
+
+public class Concert {
+    private String numeTrupa;
+    private String locatie;
+    public enum Gen {
+        Rock, Pop
+    }
+
+    private Gen gen;
+
+    private int numarBilete;
+
+    public Concert(String numeTrupa, String locatie, int numarBilete, Gen gen) {
+        this.numeTrupa = numeTrupa;
+        this.locatie = locatie;
+        this.numarBilete = numarBilete;
+        this.gen = gen;
+    }
+
+    public String getNumeTrupa() {
+        return numeTrupa;
+    }
+
+    public void setNumeTrupa(String numeTrupa) {
+        this.numeTrupa = numeTrupa;
+    }
+
+    public String getLocatie() {
+        return locatie;
+    }
+
+    public void setLocatie(String locatie) {
+        this.locatie = locatie;
+    }
+
+    public Gen getGen() {
+        return gen;
+    }
+
+    public void setGen(Gen gen) {
+        this.gen = gen;
+    }
+
+    public int getNumarBilete() {
+        return numarBilete;
+    }
+
+    public void setNumarBilete(int numarBilete) {
+        this.numarBilete = numarBilete;
+    }
+
+    @Override
+    public String toString() {
+        return "numeTrupa='" + numeTrupa + '\'' +
+                ", locatie='" + locatie + '\'' +
+                ", gen=" + gen +
+                ", numarBilete=" + numarBilete;
+    }
+}
---
+++ app/src/main/java/com/example/concertpracticeapp/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.concertpracticeapp;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioGroup;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText numeTrupaET;
+    EditText locatieET;
+    EditText numarBileteET;
+    RadioGroup radioGroup;
+    Button button;
+    ListView listView;
+
+    ArrayList<Concert> listaConcerte;
+    ArrayAdapter<Concert> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        numeTrupaET = findViewById(R.id.numeTrupaET);
+        locatieET = findViewById(R.id.locatieET);
+        numarBileteET = findViewById(R.id.numarBileteET);
+        radioGroup = findViewById(R.id.radioGroup);
+        button = findViewById(R.id.button);
+        listView = findViewById(R.id.listView);
+
+        listaConcerte = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaConcerte);
+        listView.setAdapter(adapter);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String numeTrupa = numeTrupaET.getText().toString().trim();
+                String locatie = locatieET.getText().toString().trim();
+
+                if(TextUtils.isEmpty(numeTrupa)){
+                    numeTrupaET.setError("Introduceti un nume!");
+                    numeTrupaET.requestFocus();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(locatie)) {
+                    locatieET.setError("Introduceti o locatie!");
+                    locatieET.requestFocus();
+                    return;
+                }
+
+                int numarLocuri=0;
+                try {
+                    numarLocuri = Integer.parseInt(numarBileteET.getText().toString().trim());
+                } catch (RuntimeException e) {
+                    e.printStackTrace();
+                }
+
+                if(numarLocuri > 1000){
+                    numarBileteET.setError("Numarul de bielete e prea mare!");
+                    numarBileteET.requestFocus();
+                    return;
+                }
+
+                int selectedRadioButtonId = radioGroup.getCheckedRadioButtonId();
+                Concert.Gen gen;
+
+                if(selectedRadioButtonId==R.id.rockRadioButton){
+                    gen = Concert.Gen.Rock;
+                } else
+                {
+                    gen = Concert.Gen.Pop;
+                }
+
+                if(!numeTrupa.isEmpty() && !locatie.isEmpty() && numarLocuri < 1000) {
+                    Concert concert = new Concert(numeTrupa,locatie,numarLocuri,gen);
+
+                    listaConcerte.add(concert);
+                    adapter.notifyDataSetChanged();
+
+                    numeTrupaET.setText("");
+                    locatieET.setText("");
+                    numarBileteET.setText("");
+                    numeTrupaET.requestFocus();
+                }
+            }
+        });
+
+    }
+}
